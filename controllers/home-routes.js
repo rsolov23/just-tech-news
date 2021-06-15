cd9dfa79a81d96bf90d9639ce90e441ca1253c21
Rachel Solov <rsolov23@yahoo.com>
2021-06-15 16:31:56 -0500

    finished lesson 2

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 
+// get all posts for homepage
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
     attributes: [
       "id",
@@ -32,9 +34,8 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
+
       res.render("homepage", { posts });
     })
     .catch((err) => {
@@ -42,4 +43,14 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
 module.exports = router;
